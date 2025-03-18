a6458737a737dfb5aa0f6bede054ed2243403070
psg-19 <pratushgupt22@gmail.com>
2025-03-18 21:29:57 +0530

    fixed testing of rpc.

--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@ docker-compose up -d
 # Wait for the Bitcoin RPC to be available.
 echo "Waiting for RPC to be available..."
 while true; do
-    BLOCK_HEIGHT=$(docker exec -it competency_test_p2pool-bitcoin-1 bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=18443 -rpcuser=alice -rpcpassword=password getblockheight 2>/dev/null || true)
-    if [[ -n "$BLOCK_HEIGHT" ]]; then
-        echo "RPC is available. Current block height: $BLOCK_HEIGHT"
+BLOCK_COUNT=$(sudo docker exec -it competency_test_p2pool-main-bitcoin-1 bitcoin-cli -rpcconnect=127.0.0.1 -rpcport=18443 -rpcuser=alice -rpcpassword=password getblockcount 2>/dev/null || true)
+    if [[ -n "$BLOCK_COUNT" ]]; then
+        echo "RPC is available. Current block count: $BLOCK_COUNT"
         break
     else
         echo "RPC not available yet. Retrying in 5 seconds..."
@@ -28,4 +28,4 @@ cargo run
 
 # stoping the Docker Compose services.
 echo "Rust application finished. Stopping docker-compose services..."
-docker-compose down
+sudo docker-compose down
